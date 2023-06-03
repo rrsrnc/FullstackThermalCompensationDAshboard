12fa1e3297ae0b201f6f1ac3dca4913229ebfd4e
rrsrnc <rajeev7605@gmail.com>
2023-06-03 11:18:00 +0530

    updated cors with ip of instance

--- serversocket.js
+++ serversocket.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const io = new Server(server, {
     cors:{
-        origin:"http://localhost:3000",
+        origin:"http://3.222.121.208",
         method: ["GET","POST"],
     }
 });
