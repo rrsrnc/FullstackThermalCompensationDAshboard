391e7248144df362b80895df81d3e263cd8c37d4
rrsrnc <rajeev7605@gmail.com>
2023-06-18 18:50:23 +0530

    added allowed header

--- serversocket.js
+++ serversocket.js
@@ -22,9 +22,8 @@ const io = new Server(server, {
         // origin:"http://localhost:3000",
         origin:"https://rrsrnc.github.io",
         method: ["GET","POST"],
-        headers: {
-            "Access-Control-Allow-Origin": "https://rrsrnc.github.io"
-          }
+        allowedHeaders: ["https://rrsrnc.github.io"],
+        credentials: true
     },
     path:"/socket.io",
    
